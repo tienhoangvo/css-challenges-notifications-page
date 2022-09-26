4638ce15853f1e6f4a959b9c877327fa491b329c
Tien Hoang Vo <tien.vo@dmchosting.ca>
2022-09-26 18:25:31 +0700

    fix bug: models.ts cannot be compiled

--- utils/getFormData.ts
+++
@@ -1,6 +0,0 @@
-const getFormData = <Type>(formElement: HTMLFormElement): Type => {
-  const formData = new FormData(formElement)
-  return Object.fromEntries(formData) as Type
-}
-
-export default getFormData
